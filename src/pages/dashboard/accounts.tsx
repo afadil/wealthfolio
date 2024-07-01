25a533d0cbd4acebe54188af6d578be4598a370e
Aziz FADIL <aziz.fadil@gmail.com>
2024-07-01 11:35:40 -0400

    accounts categories collapse and expand

--- src/pages/dashboard/accounts.tsx
+++ src/pages/dashboard/accounts.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Icons } from '@/components/icons';
 import { Button } from '@/components/ui/button';
@@ -38,6 +39,8 @@ const Summary = ({
   gain,
   gainPercent,
   currency,
+  isExpanded,
+  onToggle,
 }: {
   title: string;
   description?: string;
@@ -45,29 +48,29 @@ const Summary = ({
   gain: number;
   gainPercent: number;
   currency: string;
+  isExpanded: boolean;
+  onToggle: () => void;
 }) => {
   return (
-    <div key={title} className="flex items-center">
-      <div className="ml-4 space-y-1">
-        <p className="font-medium leading-none">{title}</p>
-        <p className="text-xs font-light text-muted-foreground">{description}</p>
+    <div className="flex w-full cursor-pointer items-center justify-between" onClick={onToggle}>
+      <div className="flex flex-col">
+        <div className="flex items-center">
+          <span className="font-medium leading-none">{title}</span>
+        </div>
+        <span className="text-sm text-muted-foreground">{description}</span>
       </div>
-      <div className="text-md ml-auto text-right font-medium">
-        <p className="font-medium leading-none"> {formatAmount(value, currency)}</p>
-        {gain !== 0 ? (
-          <p
-            className={`text-xs ${
-              gainPercent === 0 ? 'text-base' : gainPercent > 0 ? 'text-green-500' : 'text-red-500'
-            } `}
-          >
-            {formatAmount(gain, currency, false)} / {formatPercent(gainPercent)}
-          </p>
-        ) : null}
-      </div>
-      <div className="ml-2">
-        {/* <Button variant="ghost" size="sm">
-          <Icons.ChevronRight className="h-4 w-4" />
-        </Button> */}
+      <div className="ml-2 flex items-start justify-between">
+        <div className="flex flex-col items-end">
+          <span className="font-medium leading-none">{formatAmount(value, currency)}</span>
+          {gain !== 0 && (
+            <span className={`text-sm ${gainPercent > 0 ? 'text-green-500' : 'text-red-500'}`}>
+              {formatAmount(gain, currency, false)} / {formatPercent(gainPercent)}
+            </span>
+          )}
+        </div>
+        <Icons.ChevronDown
+          className={`ml-2 h-5 w-5 transition-transform ${isExpanded ? 'rotate-180 transform' : ''}`}
+        />
       </div>
     </div>
   );
@@ -80,7 +83,7 @@ const AccountSummary = ({ account }: { account: AccountTotal }) => {
   };
   return (
     <div key={account.id} className="flex items-center">
-      <div className="ml-4 space-y-1">
+      <div className="ml-2 space-y-1">
         <p className="font-medium leading-none">{account.name} </p>
         <p className="text-xs font-light text-muted-foreground">
           {account.group} - {account.currency}
@@ -98,8 +101,8 @@ const AccountSummary = ({ account }: { account: AccountTotal }) => {
               account.totalGainPercent === 0
                 ? 'text-base'
                 : account.totalGainPercent > 0
-                ? 'text-green-500'
-                : 'text-red-500'
+                  ? 'text-green-500'
+                  : 'text-red-500'
             } `}
           >
             {formatAmount(account.totalGainAmount, account.currency, false)} /
@@ -124,18 +127,25 @@ export function Accounts({
   className?: string;
 }) {
   const { accountsGrouped, setAccountsGrouped } = useSettingsContext();
+  const [expandedCategories, setExpandedCategories] = useState<Record<string, boolean>>({});
 
-  // Function to group accounts by category and calculate aggregate values
   const groupAccountsByCategory = () => {
     const groupedAccounts: Record<string, AccountTotal[]> = {};
     for (const account of accounts || []) {
-      const category = account.group || account.name;
+      const category = account.group || 'Uncategorized';
       if (!groupedAccounts[category]) {
         groupedAccounts[category] = [];
       }
       groupedAccounts[category].push(account);
     }
     return groupedAccounts;
+  };
+
+  const toggleCategory = (category: string) => {
+    setExpandedCategories((prev) => ({
+      ...prev,
+      [category]: !prev[category],
+    }));
   };
 
   // Component for rendering a category summary
@@ -147,30 +157,41 @@ export function Accounts({
     accountsInCategory: AccountTotal[];
   }) => {
     const categorySummary = calculateCategorySummary(accountsInCategory);
+    const isExpanded = expandedCategories[category];
+
     return (
-      <Summary
-        title={category}
-        description={
-          categorySummary.numberOfAccounts +
-          ' ' +
-          (categorySummary.numberOfAccounts > 1 ? 'accounts' : 'account')
-        }
-        value={categorySummary.totalMarketValue + categorySummary.totalCashBalance}
-        gain={categorySummary.totalGainAmount}
-        gainPercent={categorySummary.totalGainPercent}
-        currency={categorySummary.baseCurrency}
-      />
+      <div>
+        <Summary
+          title={category}
+          description={`${categorySummary.numberOfAccounts} ${
+            categorySummary.numberOfAccounts > 1 ? 'accounts' : 'account'
+          }`}
+          value={categorySummary.totalMarketValue + categorySummary.totalCashBalance}
+          gain={categorySummary.totalGainAmount}
+          gainPercent={categorySummary.totalGainPercent}
+          currency={categorySummary.baseCurrency}
+          isExpanded={isExpanded}
+          onToggle={() => toggleCategory(category)}
+        />
+        {isExpanded && (
+          <div className="mt-4 space-y-2">
+            {accountsInCategory.map((account) => (
+              <AccountSummary key={account.id} account={account} />
+            ))}
+          </div>
+        )}
+      </div>
     );
   };
 
   const renderAccounts = () => {
     if (accountsGrouped) {
       const groupedAccounts = groupAccountsByCategory();
-      return Object.keys(groupedAccounts).map((category) => (
+      return Object.entries(groupedAccounts).map(([category, accountsInCategory]) => (
         <CategorySummary
           key={category}
           category={category}
-          accountsInCategory={groupedAccounts[category]}
+          accountsInCategory={accountsInCategory}
         />
       ));
     } else {
