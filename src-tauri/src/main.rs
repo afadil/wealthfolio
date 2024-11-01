a03e78755f736365dc7e1c811ce7f837e8218454
Aziz FADIL <aziz.fadil@gmail.com>
2024-11-01 14:36:34 -0400

    add tauri log plugin

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -65,6 +65,7 @@ pub fn main() {
     dotenv().ok(); // Load environment variables from .env file if available
 
     let app = tauri::Builder::default()
+        .plugin(tauri_plugin_log::Builder::new().build())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
