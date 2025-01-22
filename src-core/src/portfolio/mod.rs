af0a98088ee08fa6cc5897597e3285854c0308c5
Aziz FADIL <aziz.fadil@gmail.com>
2025-01-22 16:17:38 -0500

    update app icon to be consistent with the market

--- src-core/src/portfolio/mod.rs
+++ src-core/src/portfolio/mod.rs
@@ -1,7 +1,7 @@
-mod history_service;
-mod holdings_service;
-mod income_service;
-// mod portfolio_calculator;
+pub mod history_service;
+pub mod holdings_service;
+pub mod income_service;
+// pub mod performance;
 pub mod portfolio_service;
 
 pub use portfolio_service::PortfolioService;
