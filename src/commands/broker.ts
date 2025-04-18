6d394ad0a2ea0b86b8b534e2294ad76c6aacacee
Hockers, Sven (Stud. DACS) <svenhockers1@gmail.com>
2025-04-18 09:34:28 +0200

    Integrated broker API update in calls

--- src/commands/broker.ts
+++ src/commands/broker.ts
@@ -1,5 +1,6 @@
 import { getRunEnv, RUN_ENV, invokeTauri } from '@/adapters';
 import { logger } from '@/adapters';
+import { HistorySummary } from '@/lib/types';
 
 export const syncBrokers = async (): Promise<void> => {
     try {
@@ -15,3 +16,22 @@ export const syncBrokers = async (): Promise<void> => {
         throw error;
     }
 };
+
+export const updatePortfolio = async (params: {
+    accountIds?: string[];
+    forceFullCalculation: boolean;
+  }): Promise<HistorySummary[]> => {
+    try {
+      switch (getRunEnv()) {
+        case RUN_ENV.DESKTOP:
+          await syncBrokers();
+          return invokeTauri('calculate_historical_data', params);
+        default:
+          throw new Error(`Unsupported`);
+      }
+    } catch (error) {
+      logger.error('Error calculating historical data.');
+      throw error;
+    }
+  };
+  
