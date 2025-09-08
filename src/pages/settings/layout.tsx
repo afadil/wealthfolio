6ea911c50292d5743914b45c5f513d7c5e067b39
Aziz FADIL <aziz.fadil@gmail.com>
2025-09-07 21:38:18 -0400

    fix styles

--- src/pages/settings/layout.tsx
+++ src/pages/settings/layout.tsx
@@ -131,17 +131,17 @@ export default function SettingsLayout() {
         )}
       </div>
 
-      {/* Desktop Layout (unchanged for desktop users) */}
+      {/* Desktop Layout */}
       <div className="hidden lg:block p-6">
         <div className="space-y-0.5">
           <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
         </div>
         <Separator className="my-6" />
-        <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-          <aside className="-mx-4 lg:w-1/6">
+        <div className="flex flex-col space-y-8 lg:grid lg:grid-cols-[220px_1fr] lg:gap-10 lg:space-y-0">
+          <aside className="lg:sticky lg:top-20 lg:self-start lg:w-[220px] lg:pr-4">
             <SidebarNav items={sidebarNavItems} />
           </aside>
-          <div className="flex-1 lg:max-w-4xl">
+          <div className="flex-1 lg:max-w-4xl min-w-0">
             <Outlet />
           </div>
         </div>
