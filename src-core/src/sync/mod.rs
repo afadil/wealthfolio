ae490a5453c14d1e2ad3405faf2cf7b4961ceb55
Aziz FADIL <aziz.fadil@gmail.com>
2025-09-08 21:09:20 -0400

    clean mobile version

--- src-core/src/sync/mod.rs
+++ src-core/src/sync/mod.rs
@@ -3,7 +3,6 @@ pub mod types;
 pub mod transport;
 // pub mod transport_tls;
 pub mod engine;
-// discovery removed in favor of explicit QR pairing
 // pub mod security;
 pub mod store;
 pub mod peer_store;
