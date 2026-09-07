59d1422fe30e569da26a1e4323f867140f6ddef2
Aziz FADIL <aziz.fadil@gmail.com>
2026-09-07 11:29:51 -0400

    chore(release): prepare version 3.9.0

--- packages/addon-sdk/src/host-dependencies.ts
+++ packages/addon-sdk/src/host-dependencies.ts
@@ -1,7 +1,7 @@
 export const HOST_DEPENDENCIES = {
   '@tanstack/react-query': '^5.90.0',
-  '@wealthfolio/addon-sdk': '^3.8.0',
-  '@wealthfolio/ui': '^3.8.0',
+  '@wealthfolio/addon-sdk': '^3.9.0',
+  '@wealthfolio/ui': '^3.9.0',
   'date-fns': '^4.1.0',
   'lucide-react': '^0.561.0',
   react: '^19.2.0',
